718602705832b5a250b05fdb75fe3267bdfb007a
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-11 22:46:33 -0600

    reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Timestamp } = require('mongodb');
+const { Schema,Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactoionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: Timestamp => new Date(timestamp).toLocaleDateString()
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema
